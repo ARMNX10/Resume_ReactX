0f3d18d478efae428cbbf92c439c757236d894dd
Aarav Maan <aaravmaan24@gmail.com>
2024-11-04 14:03:25 +0530

    updated ts

--- src/Resume.ts
+++ src/Resume.ts
@@ -1,15 +1,8 @@
 export const source = `
 $# Aarav Maan
 $ +91 7303233187 | aaravmaan24@gmail.com | <a href="https://www.linkedin.com/in/aaravmaan"> <b>LinkedIn</b></a> | <a href="https://github.com/ARMNX10"> <b>GitHub</b></a>
-# Education
-
-## Vellore Institute of Technology, Vellore. <right> Sep 2021 - Expected Jul 2025
-
-B.Tech Computer Science Engineering with Specialization in Internet of Things [ 8.02 / 10 CGPA ]
- 
-## 12th AISSCE - Dhruva Public School, New Delhi. <right> May 2019 - Jun 2021
-Science [ Maths ]
-
+# Professional Summary 
+Self-motivated and detail-oriented emerging professional with experience in Machine Learning and Software Development & Testing. Completed Amazon ML Summer School, applying CNNs and DNNs in real-world scenarios. As a software intern at Colt, identified bugs and developed test cases to enhance software quality.
 # Work Experience
 
 ## Amazon ML Summer School '24  <right> July 2024
@@ -21,7 +14,6 @@ Science [ Maths ]
 <a href = "https://drive.google.com/file/d/1uL8ju-O2v9Ft2pHialScnbSdv-hNHAM3/view?usp=sharing"><b>Software Intern</b> </a>
 - Identified and documented <b>20+ UI</b> and functional bugs, collaborated with development teams to resolve issues, leading to a decrease in reported errors on production software.
 - Coordinated with developers and product managers to align software features with user needs, achieving increased user satisfaction scores and reducing post-release bugs in <b>UAT</b>.
-- Developed detailed test plans and automated test cases, contributing to improved software quality. Collaborated with various teams to diagnose performance regressions, ensuring timely delivery.
 # Projects & Activities
 
 <h3 style="display: flex;justify-content: space-between;"> MATRIX AI Desktop Assistant <a style="font-size:1em" href="https://github.com/ARMNX10/M.A.T.R.I.X.">github.com/ARMNX10/M.A.T.R.I.X</a> </h3> 
@@ -39,14 +31,23 @@ Science [ Maths ]
 
 - Innovated a Python system, leveraging <b>OpenCV</b> and <b>Tesseract OCR</b> for live text recognition from webcam feeds.
 - Deployed spaCy for sophisticated text processing and <b>tkinter</b> for a user friendly GUI, enabling instant conversion of text to Braille and increased usability by 40% by boosting accessibility for visually impaired users.
+# Skills
+**Skills:** Python, C++, Docker, Virtualization, SQL, HTML, CSS, JavaScript, DSA,<a href="https://learn.microsoft.com/api/credentials/share/en-us/AaravMaan-2695/453B20EC1A4235DF?sharingId=F88D6CD38929C2F5"> Microsoft: Azure AI-900 </a>, GCP, AI & ML, OS & Network, MS Office.
+**Frameworks** **/** **Tools:** Pandas, NumPy, TensorFlow, PyTorch, Scikit-learn, Node.js, Selenium, Linux Shell, Hyper-V, Powershell, 
+**Languages:** English, Hindi, Deutsch (A1)
 
 # Extra-Curricular
 - Participated in HackBattle (IEEE-CS, VIT), an intense 2 day Hackathon. Built our own scripting language to maintain and extract data from different invoices. 
 - Actively participated as <a href= "https://drive.google.com/file/d/1AWulbWBss5SEor6m-LGqsMJWyceBk_de/view?usp=sharing"><b>Management Head</b></a>  in the Innovator's Quest Club, focusing on graphic design and management. Created event posters and organized five on-campus events.
-- Implemented an Announcer Bot on the club's Discord server, enhancing communication and engagement with 100+ members.
+- Implemented an Announcer Bot on the club's Discord server, enhancing communication and engagement with 100+ members. 
 
-# Skills
-**Skills:** Python, C++, Docker, Virtualization, SQL, HTML, CSS, JavaScript, DSA,<a href="https://learn.microsoft.com/api/credentials/share/en-us/AaravMaan-2695/453B20EC1A4235DF?sharingId=F88D6CD38929C2F5"> Microsoft: Azure AI-900 </a>, GCP, AI & ML, OS & Network, MS Office.
-**Frameworks** **/** **Tools:** Pandas, NumPy, TensorFlow, PyTorch, Scikit-learn, Node.js, Selenium, Linux Shell, Hyper-V, Powershell.
-**Languages:** English, Hindi, Deutsch (A1)
+
+# Education
+
+## Vellore Institute of Technology, Vellore. <right> Sep 2021 - Expected Jul 2025
+
+B.Tech Computer Science Engineering with Specialization in Internet of Things [ 8.02 / 10 CGPA ]
+ 
+## 12th AISSCE - Dhruva Public School, New Delhi. <right> May 2019 - Jun 2021
+
 `;
